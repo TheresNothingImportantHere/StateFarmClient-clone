2c04e0c89671b904f240bd43c1dccfa6ade13ec3
Hydroflame522 <154099703+Hydroflame522@users.noreply.github.com>
2024-10-15 17:47:30 -0500

    remove stinky alert (onlypuppy html better.)

--- StateFarmClient.js
+++ StateFarmClient.js
@@ -1929,9 +1929,8 @@ debug mode).`},
                 };
             };
         } catch (error) {
-            // Handle the error and display an error message onscreen
+            // alert on screen here useless, onlypuppy already made an html thing for error...
             console.error("An error occurred:", error);
-            alert("Bollocks! If you're getting this message, injection probably failed. To solve this, perform CTRL+F5 - this performs a hard reload. If this does not work, contact the developers.");
         };
     };
     const createPrompt = function(text, buttons, duration) {
